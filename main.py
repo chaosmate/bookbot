1f1c387c3d47bea939cc2edb599a007263414208
Christian Albustin <christian.albustin@googlemail.com>
2025-03-25 12:05:38 +0100

    added support for sys argv

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from stats import get_num_words, get_num_chars, get_sorted_chars
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
 
-def main():
-    string_of_words = get_book_text("books/frankenstein.txt")
+def main(path_to_book):
+    string_of_words = get_book_text(path_to_book)
     num_words = get_num_words(string_of_words)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {path_to_book}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
@@ -19,4 +24,4 @@ def main():
         print(f"{dict_pair['char']}: {dict_pair['num']}")
     print("============= END ===============")
     
-main()
+main(sys.argv[1])
